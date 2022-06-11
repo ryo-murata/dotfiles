29a81a1f3dd506f0f1b93bfb875eab45535b1eeb
faruryo <faruryo@gmail.com>
2022-06-12 01:52:29 +0900

    Remove nodebrew

--- zsh/.zprezto-contrib/faru/init.zsh
+++ zsh/.zprezto-contrib/faru/init.zsh
@@ -31,6 +31,3 @@ source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
 
 # gpg
 export GPG_TTY=$(tty)
-
-## nodebrew
-[[ -r $HOME/.nodebrew/current/bin ]] && export PATH=$HOME/.nodebrew/current/bin:$PATH
